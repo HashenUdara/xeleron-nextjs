b254bd400b20a46f25ad3143446823cab5e538a1
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2023-10-16 15:16:51 +0530

    testing with more cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -142,6 +142,294 @@ function CaseStudies({ caseStudies }) {
             </FadeIn>
           ))}
         </FadeInStagger>
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+                     <div className="mx-auto max-w-7xl ">
+        <div className='relative group'> 
+          <div className="absolute -inset-0 bg-gradient-to-r from-indigo-500 to-sky-600 rounded-3xl blur-none group-hover:blur-xl opacity-75 transition duration-1000 group-hover:duration-500"></div>
+
+          <div className="relative bg-gray-900 isolate overflow-hidden backdrop-blur-sm  px-6 pt-8 shadow-2xl rounded-3xl sm:px-8 md:pt-12 lg:gap-x-20 lg:px-12 lg:pt-0 ring-1 ring-white/10">
+  
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0 "
+              aria-hidden="true"
+            >
+              <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#4F46E5" />
+                <stop offset={1} stopColor="#80CAFF" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-10 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Boost your productivity.
+                <br />
+                Start using our app today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              </p>
+              
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10 "
+                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+                     <div className="mx-auto max-w-7xl ">
+        <div className='relative group'> 
+          <div className="absolute -inset-0 bg-gradient-to-r from-indigo-500 to-sky-600 rounded-3xl blur-none group-hover:blur-xl opacity-75 transition duration-1000 group-hover:duration-500"></div>
+
+          <div className="relative bg-gray-900 isolate overflow-hidden backdrop-blur-sm  px-6 pt-8 shadow-2xl rounded-3xl sm:px-8 md:pt-12 lg:gap-x-20 lg:px-12 lg:pt-0 ring-1 ring-white/10">
+  
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0 "
+              aria-hidden="true"
+            >
+              <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#4F46E5" />
+                <stop offset={1} stopColor="#80CAFF" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-10 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Boost your productivity.
+                <br />
+                Start using our app today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              </p>
+              
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10 "
+                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+                     <div className="mx-auto max-w-7xl ">
+        <div className='relative group'> 
+          <div className="absolute -inset-0 bg-gradient-to-r from-indigo-500 to-sky-600 rounded-3xl blur-none group-hover:blur-xl opacity-75 transition duration-1000 group-hover:duration-500"></div>
+
+          <div className="relative bg-gray-900 isolate overflow-hidden backdrop-blur-sm  px-6 pt-8 shadow-2xl rounded-3xl sm:px-8 md:pt-12 lg:gap-x-20 lg:px-12 lg:pt-0 ring-1 ring-white/10">
+  
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0 "
+              aria-hidden="true"
+            >
+              <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#4F46E5" />
+                <stop offset={1} stopColor="#80CAFF" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-10 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Boost your productivity.
+                <br />
+                Start using our app today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              </p>
+              
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10 "
+                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+                     <div className="mx-auto max-w-7xl ">
+        <div className='relative group'> 
+          <div className="absolute -inset-0 bg-gradient-to-r from-indigo-500 to-sky-600 rounded-3xl blur-none group-hover:blur-xl opacity-75 transition duration-1000 group-hover:duration-500"></div>
+
+          <div className="relative bg-gray-900 isolate overflow-hidden backdrop-blur-sm  px-6 pt-8 shadow-2xl rounded-3xl sm:px-8 md:pt-12 lg:gap-x-20 lg:px-12 lg:pt-0 ring-1 ring-white/10">
+  
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0 "
+              aria-hidden="true"
+            >
+              <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#4F46E5" />
+                <stop offset={1} stopColor="#80CAFF" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-10 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Boost your productivity.
+                <br />
+                Start using our app today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              </p>
+              
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10 "
+                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+                     <div className="mx-auto max-w-7xl ">
+        <div className='relative group'> 
+          <div className="absolute -inset-0 bg-gradient-to-r from-indigo-500 to-sky-600 rounded-3xl blur-none group-hover:blur-xl opacity-75 transition duration-1000 group-hover:duration-500"></div>
+
+          <div className="relative bg-gray-900 isolate overflow-hidden backdrop-blur-sm  px-6 pt-8 shadow-2xl rounded-3xl sm:px-8 md:pt-12 lg:gap-x-20 lg:px-12 lg:pt-0 ring-1 ring-white/10">
+  
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0 "
+              aria-hidden="true"
+            >
+              <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#4F46E5" />
+                <stop offset={1} stopColor="#80CAFF" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-10 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Boost your productivity.
+                <br />
+                Start using our app today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              </p>
+              
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10 "
+                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+                     <div className="mx-auto max-w-7xl ">
+        <div className='relative group'> 
+          <div className="absolute -inset-0 bg-gradient-to-r from-indigo-500 to-sky-600 rounded-3xl blur-none group-hover:blur-xl opacity-75 transition duration-1000 group-hover:duration-500"></div>
+
+          <div className="relative bg-gray-900 isolate overflow-hidden backdrop-blur-sm  px-6 pt-8 shadow-2xl rounded-3xl sm:px-8 md:pt-12 lg:gap-x-20 lg:px-12 lg:pt-0 ring-1 ring-white/10">
+  
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0 "
+              aria-hidden="true"
+            >
+              <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#4F46E5" />
+                <stop offset={1} stopColor="#80CAFF" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-10 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Boost your productivity.
+                <br />
+                Start using our app today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              </p>
+              
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10 "
+                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
       </Container>
     </>
   )
